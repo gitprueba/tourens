f2b26011d839adf3a2ca2f4d9064d472009cb325
gitadrian <theadn@live.com>
2014-10-16 16:46:00 -0700

    Inicio

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>TourEns</title>
+</head>
+<body>
+
+<form action="login.php" method= "post">
+Usuario <input type="text" name="user" required><br>
+Password <input type="password" name="pass" required><br>
+<input type="submit" value="Enviar">
+</form>
+
+
+</body>
+</html>
